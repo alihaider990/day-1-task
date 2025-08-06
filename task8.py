2265230cddaa5bb6cdfc27aab46fe68651639cd9
ali haider <132202541+alihaider990@users.noreply.github.com>
2025-08-05 20:26:43 -0500

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,57 @@
+#def increament(a, b=1):
+ #  return a + b
+
+#print(increament(5))
+
+
+
+
+
+
+#def factorial(n):
+ #   if n == 0 or n == 1:
+   #     return 1
+    
+ #   result = 1
+ #   for i in range(2, n + 1):
+  #      result = result * i
+    
+ #   return result
+
+
+#print(factorial(5))  
+
+#def reverse_string(k):
+ #   return k[::-1]
+#print(reverse_string("ali haider"))
+
+
+#my_list = [34,61,23,67,89,21,70]
+#def second_largest(my_list):
+ #   largest = max(my_list)
+ #   second = 0
+ #   for i in range(0, len(my_list)):
+ #       if my_list[i] < largest and my_list[i] > second:
+ #           second = my_list[i]
+ #   print("second largest number is", second)
+#second_largest(my_list) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
