09d71bc4802a56d15fbc25652ee3d93a4bc29187
alihaider990 <alihaider990@github.com>
2025-08-19 02:13:37 +0500

    capitalize first letter

--- task10.py
+++ task10.py
@@ -1,10 +1,6 @@
-def find_longest_word(sentence):
-    words = sentence.split()
-    longest_word = max(words, key=len)
-    return longest_word
-print(find_longest_word(" I am driving the at the speed of 270 km/h"))
-
-
+def capitalize_first_letter(string):
+    return string.capitalize()
+print(capitalize_first_letter("ali haider"))
 
 
 
