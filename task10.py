7421b554799a74353a7e5a7d9bce1ce831aea9ba
alihaider990 <alihaider990@github.com>
2025-08-19 02:11:25 +0500

    find the longest sentene

--- task10.py
+++ task10.py
@@ -1,10 +1,11 @@
-def check_palindrome(d):
-    if d == d[::-1]:
-        return True
-    else:
-        return False
+def find_longest_word(sentence):
+    words = sentence.split()
+    longest_word = max(words, key=len)
+    return longest_word
+print(find_longest_word(" I am driving the at the speed of 270 km/h"))
 
-print(check_palindrome("madam"))
+
+
 
 
 
