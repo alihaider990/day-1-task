12fc00c441d9cc0fa634068830b5c63bc53f888b
alihaider990 <alihaider990@github.com>
2025-08-26 04:23:44 +0500

    checking if a variable inside a loop is accessible outside the loop or not.

--- task 15/task 15.py
+++ task 15/task 15.py
@@ -1,6 +1,3 @@
-x = 5     # it is global varaiable
-
-def my_func():
-  global x
-  y = 9    # it is local variable
-  print(y)
+for i in range(3):
+    loop_var = i
+print(loop_var)  ## no block scope works in python everything is accescible after the loop are accesible outside the blocks in python
