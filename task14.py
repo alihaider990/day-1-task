fbe4328768c47f601c899e19c3ced262b52bbacb
alihaider990 <alihaider990@github.com>
2025-08-25 20:59:53 +0500

    leet code question: checking the index of a number between the list then at the end of the list.

--- task14.py
+++ task14.py
@@ -1,6 +1,10 @@
+my_list = [1, 3, 4, 6]
+targets = [7, 2]
 
-
-
-BIG O NOTATION is basically timecomplexcity like how much time or steps/efficiency it will take to perfrom the task through algorithms_available
-linear search: is basically used for small problems like finding an index of an number of list of 6 MemberDescriptorType
-binary search : is basically used for complex problems in which list must be sorted , unsorted list will not work here it will be used to perform complex tasks like if we wanna find 56 floor in a building of 100 floor, and finding something in an book or dictionary. 
+for target in targets:
+    for i in range(len(my_list)):
+        if my_list[i] >= target:
+            print(i)
+            break
+    else:
+        print(len(my_list))
