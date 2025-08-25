6d345adb9ddf4a27a92144fc10a0b735da54ce86
alihaider990 <alihaider990@github.com>
2025-08-26 03:55:39 +0500

    global vs local varaibles.

--- task 15/task 15.py
+++
@@ -1,6 +0,0 @@
-x = 5     # it is global varaiable
-
-def my_func():
-  global x
-  y = 9    # it is local variable
-  print(y)
--- task15/task15.py
+++
@@ -1,6 +0,0 @@
-print("Problem 2")
-
-if 2 == 2:
-    print("True")
-else:
-    print("False")
