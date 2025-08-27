008f63cfeaa4012b437b798eb8044581b92c413b
alihaider990 <alihaider990@github.com>
2025-08-27 23:50:51 +0500

    Done with first problem basic promise

--- task16/task16.py
+++ task16/task16.py
@@ -2,37 +2,23 @@ import asyncio
 
 async def my_promise(success):
     if success:
-        return True
+        return "Task completed successfully "
     else:
-        raise Exception("error message")
+        raise Exception("Task failed ")
 
 async def main():
+    
     try:
         result = await my_promise(True)
         print(result)  
     except Exception as error:
         print(error)
-
-asyncio.run(main())
-
-
-async def my_promise(success):
-    if success:
-        return True
-    else:
-        raise Exception("error message")
-
-async def main():
+    
+     
     try:
-        result = await my_promise(false)
+        result = await my_promise(False) 
         print(result)  
     except Exception as error:
         print(error)
 
-asyncio.run(main())
-
-
-
-
-
-
+asyncio.run(main()) 
