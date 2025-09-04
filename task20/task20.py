eec7bd70de63dfe006720de198a5d2c7f10983dc
alihaider990 <alihaider990@github.com>
2025-09-04 17:34:20 +0500

    mini project: Creating a Book class with title, author, year then adding books then removing 1 book after that it will show the remaining books left.

--- task20/task20.py
+++ task20/task20.py
@@ -1,21 +1,46 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
 
-class Student(Person):  
-    def __init__(self, name, grade):
-        super().__init__(name)
-        self.grade = grade  
+    def __str__(self):
+        return f'"{self.title}" by {self.author} ({self.year})'
 
-    @staticmethod
-    def compare(student1, student2):
-        if student1.grade > student2.grade:
-            return f"{student1.name} has a higher grade."
-        elif student1.grade < student2.grade:
-            return f"{student2.name} has a higher grade."
-student1 = Student("Ali", 90)
-student2 = Student("Hassan", 85)
+class Library:
+    def __init__(self):
+        self.books = []
 
-result = Student.compare(student1, student2)
-print(result)  
+    def add_book(self, book):
+        self.books.append(book)
+        print(f'Added: {book}')
+
+    def remove_book(self, title):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                print(f'Removed: {book}')
+                return
+       
+
+    def show_books(self):
+        if not self.books:
+            print("No books in the library.")
+        else:
+            print("Library books:")
+            for book in self.books:
+                print(book)
+
+
+library = Library()
+
+book1 = Book("art of everything", "harryporter", 2018)      
+book2 = Book("how to make money", "changez", 2008)   
+
+library.add_book(book1)
+library.add_book(book2)
+library.show_books()
+library.remove_book("art of everything")  
+library.show_books()
+      
      
