64e3fbd3e178bdfa0a8d6f0c1fded904713f79ce
alihaider990 <alihaider990@github.com>
2025-09-09 01:36:46 +0500

    leet code task: check valid parenthesis.

--- task21/task21.py
+++ task21/task21.py
@@ -1,28 +1,25 @@
-class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__balance = initial_balance  
+def isvalid(s: str) -> bool:
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
-        else:
-            print("Deposit amount must be positive.")
+    for char in s:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping:
+            if not stack or stack[-1] != mapping[char]:
+                return False
+            stack.pop()
+    return not stack
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
-            self.__balance -= amount
-        else:
-            print("Insufficient funds or invalid amount.")
-
-    @property
-    def balance(self):
-        return self.__balance
+print(isvalid("()"))
+print(isvalid("()[]{}"))
+print(isvalid("(]"))
 
 
-account = BankAccount(100)
-account.deposit(50)
-account.withdraw(30)
-print("Current balance:", account.balance)  
+
+
+
+
 
 
 
