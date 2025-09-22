745629eca5ec5135070a8683b2b8884f1110a556
alihaider990 <alihaider990@github.com>
2025-09-23 03:53:05 +0500

    task : Movie ticket booking system.

--- task25/task25.py
+++ task25/task25.py
@@ -10,6 +10,52 @@ for i in range(len(my_list)):
 
 
 
+class Movie:
+    def __init__(self, movie_name, movie_id, available_seats, price_per_ticket):
+        self.movie_name = movie_name
+        self.movie_id = movie_id
+        self.available_seats = available_seats
+        self.price_per_ticket = price_per_ticket
+
+class cinemahall:
+    def __init__(self):
+        self.movies = {}
+        self.total_rev = 0
+
+    def add_movie(self, movie):
+        self.movies[movie.movie_id] = movie
+
+    def book_ticket(self, movie_id, num_tickets):
+        if movie_id in self.movies:
+            movie = self.movies[movie_id]
+          
+            if movie.available_seats >= num_tickets:
+                movie.available_seats = movie.available_seats - num_tickets
+                cost = num_tickets * movie.price_per_ticket
+                self.total_rev = self.total_rev + cost
+                print("Booking successful")
+                return cost  
+            else:
+                print("Not enough seats")
+                return None   
+
+    def get_total_rev(self):
+        return self.total_rev
+
+movie1 = Movie("Fast and Furious", 1, 10, 100)
+movie2 = Movie("Conjuring", 2, 10, 100)
+
+cinema = cinemahall()
+cinema.add_movie(movie1)
+cinema.add_movie(movie2)
+
+cost1 = cinema.book_ticket(1, 2)
+print("Cost:", cost1)
+
+cost2 = cinema.book_ticket(2, 3)
+print("Cost:", cost2)
+print("Total Rev:", cinema.get_total_rev())
+
 
 
 
