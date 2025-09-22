53b4c0c6915e747e3d249ffe701352fb63f90100
alihaider990 <alihaider990@github.com>
2025-09-23 01:20:04 +0500

    leet code question: first finding index of 5 by adding 1 and 4 then making 0 index to 1.

---
+++ task25/task25.py
@@ -0,0 +1,31 @@
+my_list = [1,4,6,7,8]
+target = 5
+
+for i in range(len(my_list)):
+    for j in range(i+1,len(my_list)):
+        if my_list[i] + my_list[j] == target:
+            print({i + 1, j + 1})
+
+            # time complexcity is 0(n)
+
+
+
+
+
+
+
+
+
+                
+
+            
+
+
+
+    
+
+    
+
+
+
+
