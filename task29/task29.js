0d98aee9a076bb99ab50e5e365c704985fabfad7
alihaider990 <alihaider990@github.com>
2025-10-10 02:22:39 +0500

    task 29: updated.

--- task29/task29.js
+++ task29/task29.js
@@ -1,4 +1,4 @@
-class movieratingdashboard {
+class   Movieratingdashboard {
     constructor() {
         this.movies = [];
     }
@@ -12,27 +12,36 @@ class movieratingdashboard {
         };
         this.movies.push(movie);
     }
+    getAverageRating(movieId) {
+        const movie = this.movies.find(m => m.id === movieId);
+        if (!movie || movie.ratings.length === 0) return 0;
+        const sum = movie.ratings.reduce((a, b) => a + b, 0);
+        return +(sum / movie.ratings.length).toFixed(1);
+    }
 
     addrating(movieId, rating) {
         const movie = this.movies.find(m => m.id === movieId);
         if (movie) {
             movie.ratings.push(rating);
+            const sum = movie.ratings.reduce((a,b) => a+b,0);
+            const avg = sum/movie.ratings.length;
+            movie.avgRating = +(sum/movie.ratings.length).toFixed(1);
         }
     }
 }
 
-const movies = new movieratingdashboard();
+const movies = new Movieratingdashboard();
 
-    movies.addmovie("batman", "Action");
-    movies.addmovie("spider man", "Action");
-    movies.addmovie("spencer confidential", "Action");
-    movies.addmovie("fast and furious", "Action");
-    movies.addmovie("formula 1", "racing");
-    movies.addmovie("the karate kid", "Action");
-    movies.addmovie("commondo  ", "Action");
-    movies.addmovie("saiyaara", "romance");
-    movies.addmovie("tiger zinda hai", "Action");
-    movies.addmovie("sultan", "Action");
+movies.addmovie("batman", "Action");
+movies.addmovie("spider man", "Action");
+movies.addmovie("spencer confidential", "Action");
+movies.addmovie("fast and furious", "Action");
+movies.addmovie("formula 1", "racing");
+movies.addmovie("the karate kid", "Action");
+movies.addmovie("commondo  ", "Action");
+movies.addmovie("saiyaara", "romance");
+movies.addmovie("tiger zinda hai", "Action");
+movies.addmovie("sultan", "Action");
 
 movies.addrating(1, 5);
 movies.addrating(1, 4);
@@ -47,7 +56,6 @@ movies.addrating(5, 3);
 movies.addrating(6, 5);
 movies.addrating(6, 1);
 
-
 function display_movie_details() {
     const ratedMovies = movies.movies
         .filter(movie => movie.ratings.length > 0)
@@ -59,28 +67,33 @@ function display_movie_details() {
     const top3 = [...ratedMovies].sort((a, b) => b.avgRating - a.avgRating).slice(0, 3);
     const lowestRated = [...ratedMovies].sort((a, b) => a.avgRating - b.avgRating)[0];
 
+    const genrecount = {};
+    for (let movie of ratedMovies) {
+        genrecount[movie.genre] = (genrecount[movie.genre] || 0) + 1;
+    }
+    
+
     let commongenere = null;
     let maxcount = 0;
     
     for(let genre in genrecount){
-if(genrecount[genre] > maxcount){
-    maxcount = genrecount[genre];
-    commongenere = genre;
-}
+        if(genrecount[genre] > maxcount){
+            maxcount = genrecount[genre];
+            commongenere = genre;
+        }
     }
 
-    console.log("\nTop 3 Movies:");
-    top3.forEach((movie, i) => {
-        console.log(`${i + 1}. ${movie.title} (${movie.avgRating})`);
-    });
-
-    console.log(`\nLow Rated Movies: ${lowestRated.title} (${lowestRated.avgRating})`);
-    console.log(`\nMost Common Genre: ${commongenere}`);
+    console.log("\n🎬 Top 3 Movies by Rating:");
+    top3.forEach((movie, i) => console.log(`${i + 1}. ${movie.title} (${movie.avgRating})`));
+    
+    console.log(`\n  Lowest Rated Movie: ${lowestRated.title} (${lowestRated.avgRating})`);
+    console.log(`\n  Most Common Genre: ${commongenere}`);
 }
+
 console.table(movies.movies);
 
-
 display_movie_details();
+
 
 
 
